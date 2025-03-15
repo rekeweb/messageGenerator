26613229eb7e1fd1a07224628cfc6ccf6375ed33
rekeweb <rekeweb91@gmail.com>
2025-03-15 10:04:08 +0800

    many changes git status q

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
 const messageGenerator = {
-    _nama: 'nama',
+    _nama: 'a',
     _age: 0,
-    set nama(newName) {
-        this._nama = newName;
+    namaArr: ['ahmad', 'ali', 'abu'],
+    ageArr: [12, 46, 142],
+    nama() {
+        let namaIndex = Math.floor(Math.random() * 3);
+        this._nama = this.namaArr[namaIndex];
     },
-    set age(newAge) {
-       this._age = newAge; 
+    age() {
+        let ageIndex = Math.floor(Math.random() * 3);
+        this._age = this.ageArr[ageIndex];
     },
-    get details() {
-        return `Nama: ${this._nama}\nUmur: ${this._age}`;
+    details() {
+        return console.log(`Nama: ${this._nama}\nUmur: ${this._age}`);
     },
-    morningTips: [],
-    afternoonTIps: [],
-    eveningTips: []
+    message() {
+        let morningIndex = Math.floor(Math.random() * 3);
+        let afternoonIndex = Math.floor(Math.random() * 3);
+        let eveningIndex = Math.floor(Math.random() * 3);
+        return console.log(`Morning Tips: ${this.morningTips[morningIndex]}.\nAfternoon Tips: ${this.afternoonTips[afternoonIndex]}.\nEvening Tips: ${this.eveningTips[eveningIndex]}.`)
+    },
+    morningTips: ['Base your meals on higher fibre starchy carbohydrates', 'Eat lots of fruit and veg', 'Eat more fish, including a portion of oily fish'],
+    afternoonTips: ['Cut down on saturated fat and sugar','Eat less salt: no more than 6g a day for adults', 'Get active and be a healthy weight'],
+    eveningTips: ['Do not get thirsty', 'Do not skip breakfast', 'cheat day once in a while :)']
 };
 
 
 
 
-const person = (nama, umur) => {
-    messageGenerator.nama = nama;
-    messageGenerator.age = umur;
-    console.log(messageGenerator.details);
+const person = () => {
+    let i = 0;
+    while (i < 3) {
+    messageGenerator.nama();
+    messageGenerator.age();
+    messageGenerator.details();
+    messageGenerator.message();
+    i++;
+    }
 };
 
-const ahmad = person('ahmad', 38);
-
-console.log(ahmad);
+person();
