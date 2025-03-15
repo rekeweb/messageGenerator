024b1f8a2c49c1da9e29f50296e87d9900a1d4b4
rekeweb <rekeweb91@gmail.com>
2025-03-15 08:50:17 +0800

    latest features

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-const addTwo = x => x + 2;
+const messageGenerator = {
+    _nama: 'nama',
+    _age: 0,
+    set nama(newName) {
+        this._nama = newName;
+    },
+    set age(newAge) {
+       this._age = newAge; 
+    },
+    get details() {
+        return `Nama: ${this._nama}\nUmur: ${this._age}`;
+    },
+    morningTips: [],
+    afternoonTIps: [],
+    eveningTips: []
+};
 
-console.log(addTwo(10));
+
+
+
+const person = (nama, umur) => {
+    messageGenerator.nama = nama;
+    messageGenerator.age = umur;
+    console.log(messageGenerator.details);
+};
+
+const ahmad = person('ahmad', 38);
+
+console.log(ahmad);
